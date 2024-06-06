8c9c82309affd06d4c13694ee1407f6466488b4d
wiut-00012605 <johndoe4773013@gmail.com>
2024-06-06 13:53:50 +0500

    first commit

---
+++ courseWork-API/Repository/ProductRepository.cs
@@ -0,0 +1,50 @@
+﻿using courseWork_API.DBContexts;
+using courseWork_API.Model;
+using System.Data.Entity;
+
+namespace courseWork_API.Repository
+{
+    public class ProductRepository: IProductRepository
+    {
+        private readonly ProductContext _dbContext;
+        public ProductRepository(ProductContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void Save()
+        {
+            _dbContext.SaveChanges();
+        }
+
+        public void DeleteProduct(int productId)
+        {
+            var product = _dbContext.Products.Find(productId);
+            _dbContext.Products.Remove(product);
+            Save();
+        }
+        public Product GetProductById(int productId)
+        {
+            var prod = _dbContext.Products.Find(productId);
+            return prod;
+        }
+        public IEnumerable<Product> GetProducts()
+        {
+            return _dbContext.Products.ToList();
+        }
+        public void InsertProduct(Product product)
+        {
+
+            _dbContext.Add(product);
+            Save();
+        }
+        
+        public void UpdateProduct(Product product)
+        {
+            _dbContext.Entry(product).State =
+            Microsoft.EntityFrameworkCore.EntityState.Modified;
+            Save();
+        }
+
+    }
+}
